435ba90594ae076fdc1664ac42a650c9718563f8
jmurillov1 <gatitojm.17@hotmail.com>
2021-06-13 17:49:37 -0500

    Mofificamos el SW

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 var url = window.location.href;
-var swLocation = "twittor/sw.js";
+var swLocation = "/twittor/sw.js";
 if (navigator.serviceWorker) {
   if (url.includes("localhost")) {
-    swLocation = "sw.js";
+    swLocation = "/sw.js";
   }
   navigator.serviceWorker.register(swLocation);
 }
